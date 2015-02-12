22f3e984cd42f4377636c3cb624af93572ec56b7
manetsberger <manetsberger90@gmail.com>
2015-02-12 13:40:52 -0800

    Fantasy Game

---
+++ scripts.js
@@ -0,0 +1,24 @@
+var visitor = prompt('Enter a Your Character Name');
+var characterClass = prompt('What is your Character Class?');
+var message = 'Hello ' + visitor + ', the Mighty ' + characterClass;
+document.write(message);
+
+
+var weapon = prompt(' Enter your Weapon ');
+var message2 = " We are so glad you came by to visit our town, Arindal, wielding your weapon: " + weapon;
+document.write(message2);
+
+
+var pathinLife = prompt(". With such a powerful weapon! In a World, where we must make ourselves, what path in life do you choose?");
+var message3 = ". With such a powerful weapon! In a World, where we must make ourselves, what path in life do you choose? " + pathinLife;
+document.write(message3);
+
+
+var answer4 = ". Excellent, the entire village is relying upon you! ";
+document.write(answer4);
+
+var answer5 = prompt("What is the next step in your journey?");
+var message4 = "What is the next step in your journey? ";
+
+document.write(message4 + answer5);
+document.write("<p>Good Answer</p>");
